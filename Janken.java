c41da7e385f2a3e2af2acc5c9f660c2b0ace8144
KouheiS <kouhei.s.473@gmail.com>
2014-03-04 19:54:31 +0900

    var3

--- Janken.java
+++ Janken.java
@@ -37,33 +37,8 @@ class Player{
 	}
 }
 
-
-
-
-class Janken{
-	public static void main(String[] args){
-		System.out.println("【ジャンケン開始】");
-		Player player1 = new Player();
-		Player player2 = new Player();
-
-		int p1Hand, p2Hand;
-
-		for(int i=0; i<3; i++){						//三回勝負
-			System.out.println("【" + i + "回戦目】");
-
-			p1Hand = player1.pose();
-			p2Hand = player2.pose();
-
-			Hantei(p1Hand, p2Hand, player1, player2);
-
-		}
-
-		System.out.println("【ジャンケン終了】");
-
-		Winer(player1, player2);
-	}
-
-	static void Hantei(int p1Hand, int p2Hand, Player player1, Player player2){
+class Manager{
+	public void Hantei(int p1Hand, int p2Hand, Player player1, Player player2){
 		if(p1Hand == p2Hand){					//あいこの場合
 				System.out.println(player1.hand() + "VS" + player2.hand());
 				System.out.println("あいこです。");
@@ -89,7 +64,7 @@ class Janken{
 			}
 	}
 
-	static void Winer(Player player1, Player player2){
+	public void Winer(Player player1, Player player2){
 		if(player1.Result() == player2.Result()){
 			System.out.println("引き分けです。");
 		}else if(player1.Result() > player2.Result()){
@@ -97,5 +72,33 @@ class Janken{
 		}else{
 			System.out.println(""+player1.Result() + "対" +"" +player2.Result() +"でプレイヤー２の勝利です。");
 		}
+	}
+}
+
+
+
+
+class Janken{
+	public static void main(String[] args){
+		System.out.println("【ジャンケン開始】");
+		Player player1 = new Player();
+		Player player2 = new Player();
+		Manager manager = new Manager();
+
+		int p1Hand, p2Hand;
+
+		for(int i=0; i<3; i++){						//三回勝負
+			System.out.println("【" + i + "回戦目】");
+
+			p1Hand = player1.pose();
+			p2Hand = player2.pose();
+
+			manager.Hantei(p1Hand, p2Hand, player1, player2);
+
+		}
+
+		System.out.println("【ジャンケン終了】");
+
+		manager.Winer(player1, player2);
 	}
 }
